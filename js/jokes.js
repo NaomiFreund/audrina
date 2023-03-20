bf11e254c9342cf4e63d5ea0adae76492e64f910
Naomi Freund <111409257+NaomiFreund@users.noreply.github.com>
2023-03-20 21:41:30 +0000

    Mon, Mar 20, 2023, 2:41 PM -07:00

--- js/jokes.js
+++ js/jokes.js
@@ -1,1 +1,8 @@
- 
+ const jokeE1 = document.getElementById('joke')
+ const jokeBtn = document.getElementById('jokeBtn')
+
+ jokeBtn.addEventListener('click', generateJoke)
+
+ function generateJoke(){
+    
+ }
