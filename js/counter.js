432334bf43bbb8d98a716b78b9ba7d5b2b67692d
Naomi Freund <111409257+NaomiFreund@users.noreply.github.com>
2023-03-21 21:51:51 +0000

    Tue, Mar 21, 2023, 2:51 PM -07:00

--- js/counter.js
+++ js/counter.js
@@ -5,7 +5,14 @@ counters.forEach(counter => {
 
     const updateCounter = () => {
         const target = counter.getAttribute('data-target')
-        console.log(target)
+        const c = +counter.innerText
+
+        const increment = 1
+
+        if(c < target) {
+            counter.innerText = `${Math.ceil(c + increment)}`
+            set
+        }
     }
 
     updateCounter()
